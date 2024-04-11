5446e5c5f6d8ce4e77f67c38e849bf5b5bdaf90a
drhaoloi <hcloi@wpi.edu>
2024-04-11 13:41:26 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+int countDown(int n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        n = n + countDown(n - 1);
+        return n;
+    }
+}
+
+void countCharacter(string str) {
+    if (str.length() == 0) {
+        return;
+    } else {
+
+        countCharacter(str.substr(1));
+        cout << str[0] << endl;
+    }
+}
+
+string sumString(string str) {
+    if (str.length() == 0) {
+        return "";
+    } else {
+
+        return sumString(str.substr(1)) + str[0];
+
+    }
+}
+
+#include <iostream>
+using namespace std;
+
+int search(int arr[], int low, int high, int key) {
+    if (low > high) {
+        return -1;
+    }
+
+    if (arr[low] == key) {
+        return low;
+    }
+
+    if (arr[high] == key) {
+        return high;
+    }
+
+    return search(arr, low + 1, high - 1, key);
+}
+
+int binary_search(int arr[], int low, int high, int key) {
+    if (low > high) {
+        return -1;
+    }
+
+    int mid = (low + high) / 2;
+
+    if (arr[mid] == key) {
+        return mid;
+    }
+
+    if (arr[mid] > key) {
+        return binary_search(arr, low, mid - 1, key);
+    } else {
+        return binary_search(arr, mid + 1, high, key);
+    }
+}
+
+
+int main() {
+    // create a sorted array with 10 elements
+int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int key = 5;
+cout << "Linear search: " << search(arr, 0, 9, key) << endl;
+
+    return 0;
+}
+//int main() {
+//    // cout << countDown(5) << endl;
+//    // countCharacter("Hello");
+//    string s = sumString("Hello");
+//    cout << s << endl;
+//    return 0;
+//}
