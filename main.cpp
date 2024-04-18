dd5a35293ffd3e044dcd9a0fc39f61cd39c4b4fe
drhaoloi <hcloi@wpi.edu>
2024-04-18 13:30:58 -0400

    print

--- main.cpp
+++ main.cpp
@@ -82,6 +82,16 @@ int binary_search(int arr[], int low, int high, int key) {
     }
 }
 
+void print(vector<string> v) {
+    if (v.empty()) {
+        return;
+    } else {
+        cout << v[0] << endl;
+        v.erase(v.begin());
+        print(v);
+
+    }
+}
 
 int main() {
     // create a sorted array with 10 elements
@@ -92,9 +102,8 @@ int main() {
     vector<string> v = {"am", "bcc", "cd", "d", "echo", "f", "g", "h", "i", "j"};
 
     vector<string> v1 = accumString(v, 2);
-    for (string s: v1) {
-        cout << s << endl;
-    }
+    print(v1);
+    return 0;
 }
 //int main() {
 //    // cout << countDown(5) << endl;
